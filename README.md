a5df035fe4b2611b8dc0704477b5619de11fb9e3
Moritz Schmitz von Hülst <moritz@hauptstadtoffice.com>
2021-02-07 14:45:24 +0100

    some webcrawling test

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # stocksonlygoup
 
-Hello World!
+## backend
+
+### start
+
+cd backend && npm start
+
+### get stock
+
+GET http://localhost:3000/stocks/{SYMBOL}
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+const puppeteer = require('puppeteer');
+
+const prices = {};
+
+const getStockPrice = async (symbol) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://www.google.com/search?q=${symbol}+stock`, {waitUntil: 'domcontentloaded'});
+    const evaluate = await page.evaluate(
+        () => {
+            const element = document.querySelector("g-card-section > span");
+            if (element) {
+                return element.textContent;
+            }
+        }
+    );
+    await browser.close();
+    return evaluate;
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/stocks/:symbol', async function (req, res, next) {
+    const {symbol} = req.params;
+
+    let price = prices[symbol];
+    if (price === undefined) {
+        prices[symbol] = await getStockPrice(symbol);
+        price = prices[symbol];
+    }
+
+    res.send({price, symbol});
 });
+
+setInterval(() => {
+    Object.keys(prices).forEach(async (symbol) => {
+        prices[symbol] = await getStockPrice(symbol);
+    });
+}, 1000 * 60);
 
 module.exports = router;
